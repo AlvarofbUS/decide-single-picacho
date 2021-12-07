3a81d2bac527e3631b32cfd34827aa2ab920452b
Alvaro <alvfribal@alum.us.es>
2021-12-07 19:46:20 +0100

    Añadida url para la nueva funcionalidad en cabina

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import BoothView
-
+from .views import BoothView, BoothListView
 
 urlpatterns = [
     path('<int:voting_id>/', BoothView.as_view()),
+    path('', BoothListView.as_view())
 ]
