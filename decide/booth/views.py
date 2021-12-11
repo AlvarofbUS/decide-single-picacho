56451a703c9f700fb6ecf43b44d65788bb4eae05
AlvarofbUS <alvfribal@alum.us.es>
2021-12-11 16:42:30 +0100

    (Feature) Añadidos metodos para nueva funcionalidad cabina
    ----------------------
    Añadidos los dos metodos necesarios para realizar la nueva funcionalidad implementada en cabina.
    Posee logs para poder probar por los diferentes compañeros

--- decide/booth/views.py
+++ decide/booth/views.py
@@ -2,8 +2,13 @@ import json
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from base import mods
+from census.models import Census
+from voting.models import Voting
+from voting.serializers import VotingSerializer
 
 
 # TODO: check permissions and census
@@ -13,10 +18,8 @@ class BoothView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         vid = kwargs.get('voting_id', 0)
-
         try:
             r = mods.get('voting', params={'id': vid})
-
             # Casting numbers to string to manage in javascript with BigInt
             # and avoid problems with js and big number conversion
             for k, v in r[0]['pub_key'].items():
@@ -27,5 +30,43 @@ class BoothView(TemplateView):
             raise Http404
 
         context['KEYBITS'] = settings.KEYBITS
-
+        print(context)
         return context
+
+
+class BoothListView(TemplateView):
+    template_name = 'booth/booth_list.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        print('--------------------------')
+        print(context)
+        print('--------------------------')
+        context['KEYBITS'] = settings.KEYBITS
+        return context
+
+
+class VotingList(APIView):
+    def post(self, request):
+        idUser = request.user.id
+        print('idUser: ', idUser)
+
+        votacionesCensus = Census.objects.filter(voter_id = idUser)
+        print('Votaciones Census: ', votacionesCensus)
+        idsVotaciones = []
+        for v in votacionesCensus:
+            idsVotaciones.append(v.voting_id)
+        
+        print('ids votaciones: ', idsVotaciones)
+
+        votaciones = []
+        for idVoting in idsVotaciones:
+            votacion = mods.get('voting', params={'id': idVoting})
+            votaciones.append(votacion)
+            print('--------------------------')
+            print(votacion)
+
+        print('--------------------------')
+        print(votaciones)
+        print('--------------------------')
+        return Response(votaciones)
