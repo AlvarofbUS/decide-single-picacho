f18c9a560cd8c63340848b3d9945f16cc41bd5cb
luicharom10 <luicharom@alum.us.es>
2022-01-02 13:17:15 +0100

    (Test) Test de carga en postprocesado --------------------- Se ha implementado un test de carga dentro de loadtest en locustfile.py para comprobar como funciona el modulo cuando tiene que gestionar mas votaciones.

--- loadtest/locustfile.py
+++ loadtest/locustfile.py
@@ -62,6 +62,24 @@ class DefVoters(SequentialTaskSet):
     def on_quit(self):
         self.voter = None
 
+class DefPostproc(SequentialTaskSet):
+
+    def on_start(self):
+        with open('votings.json') as f:
+            self.votings = json.loads(f.read())
+            self.voting = choice(list(self.votings.items()))
+
+    @task
+    def do_postproc(self):
+        json = self.voting
+        data = json[1][0]
+        print(data)
+        self.client.post('/postproc/', json=data)
+
+    def on_quit(self):
+        self.voting = None
+
+
 class Visualizer(HttpUser):
     host = HOST
     tasks = [DefVisualizer]
@@ -73,3 +91,9 @@ class Voters(HttpUser):
     host = HOST
     tasks = [DefVoters]
     wait_time= between(3,5)
+
+
+class Postproc(HttpUser):
+    host = HOST
+    tasks = [DefPostproc]
+    wait_time = between(3, 5)
