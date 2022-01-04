d2617adc91abd3b5d42b76b142cdba37f1979dc1
luicharom10 <luicharom@alum.us.es>
2022-01-04 15:55:25 +0100

    (Test) Implementando test ley simple --------------------- Implementando en tests.py del modulo de postproc el test correspondiente para comprobar el buen funcionamiento del metodo

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -63,3 +63,32 @@ class PostProcTestCase(APITestCase):
 
         values = response.json()
         self.assertEqual(values, expected_result)
+    
+    def test_simple(self):
+        data = {
+            'type': 'SIMPLE',
+            'escanio': 7,
+            'options': [
+                { 'option': 'Option 1', 'number': 1, 'votes': 5 }, 
+                { 'option': 'Option 2', 'number': 2, 'votes': 2 },
+                { 'option': 'Option 3', 'number': 3, 'votes': 5 },
+                { 'option': 'Option 4', 'number': 4, 'votes': 2 },
+                { 'option': 'Option 5', 'number': 5, 'votes': 5 },
+                { 'option': 'Option 6', 'number': 6, 'votes': 1 },
+            ]
+        } 
+        
+        expect_result = [
+            { 'option': 'Option 1', 'number': 1, 'votes': 5, 'escanio': 2 },
+            { 'option': 'Option 3', 'number': 3, 'votes': 5, 'escanio': 2 },
+            { 'option': 'Option 5', 'number': 5, 'votes': 5, 'escanio': 2 },
+            { 'option': 'Option 2', 'number': 2, 'votes': 2, 'escanio': 1 },
+            { 'option': 'Option 4', 'number': 4, 'votes': 2, 'escanio': 0 },
+            { 'option': 'Option 6', 'number': 6, 'votes': 1, 'escanio': 0 },
+            ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+    values = response.json()
+        self.assertEqual(values, expect_result)
